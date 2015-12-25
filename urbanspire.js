955f3295c7ec7e40bbceb863c0772697e1815af2
Asli Kimya <akimya@stanford.edu>
2015-12-25 12:34:56 +0200

    Added platform files

---
+++ urbanspire.js
@@ -0,0 +1,234 @@
+// Constants
+var p4 = {teacher_name:"Margaret", class_name:"Italian Art", description: "Learn the ancient artistic tradition of Italian Art. Beginning with the Neolithic age, Ill take you through the hisory of Italian art through the present day. Come ready to be amazed and to appreciate the beauty Italian artists have created for us.", photo: "photos/hands.png"};
+var p5 = {teacher_name:"Asli", class_name:"Korean Dance", description: "Dance in Korea began over 5,000 years ago with shamanistic early rituals. We will explore the deep history of Korean dance, and focus on Folk dance, a style the blends the old and the new", photo: "photos/korean_women.png"};
+var p6 = {teacher_name:"Margaret", class_name:"Russian Crafts", description: "Matryoshka dolls represent the matryoshka principle that remindes of of relationship of objects within similar objects, a relationship that appears in nature other art pieces.  We will discuss the significance of the dolls,and use them as a starting point to understand the rich art and culture of Russia.", photo: "photos/dolls.png"};
+var p7 = {teacher_name:"Asli", class_name:"South Asian Painting", description: "Experience the history of South Asia while learning the painting craft.  I paint every weekend, at least, and see no greater joy than sharing my talents with others.", photo: "photos/painting.png"};
+var p8 = {teacher_name:"Nikhita", class_name:"South African Weaving", description: "Learn the beauty and color of Africa through a weaving class. I know we will enjoy chatting about history and our lives in San Francisco.  Weaving is the perfect activity to share with a new friend", photo: "photos/weavebowl.jpg"};
+var p9 = {teacher_name:"Margaret", class_name:"Mediterranean Cooking", description: "Discover the wonders of Mediterranean  cuisine.  My Big Fat Greek Wedding was only the start of all the fun.  Let me know ahead of time if there are any specifics you want to cover, and I'll make sure we're covered.", photo: "photos/greekfood.png"};
+var korean_dance = {teacher_name:"Asli", class_name:"Korean Dance", description: "Dance in Korea began over 5,000 years ago with shamanistic early rituals. We will explore the deep history of Korean dance, and focus on Folk dance, a style the blends the old and the new", photo: "photos/korean_women.png"};
+var turkish_dance = {teacher_name:"Asli", class_name:"Turkish Belly Dance", description: "Haven't you always wondered the tricks to being a beautiful and skilled belly dancer? I've been dancing and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to move!", photo: "photos/turkish_dance.png"};
+var japanese_dance = {teacher_name:"Asli", class_name:"Japenese Dancing", description: "Japan is a land like no other.  The best way I know to share the beauty of my home country is through the performing arts.  Come for an afternoon and let me share the joy I experience from Japanese dance with you.", photo: "photos/japan.png"};
+var greek_food = {teacher_name:"Margaret", class_name:"At Home Greek Cuisine", description: "Discover the wonders of greek  cuisine.  My Big Fat Greek Wedding was only the start of all the fun.  Let me know ahead of time if there are any specifics you want to cover, and I'll make sure we're covered. ", photo: "photos/greekfood.png"};
+var turkish_food = {teacher_name:"Asli", class_name:"Turkish Holiday Meal", description: "Let me share the rich flavors and history of Turkey with you through holiday cooking.  My hope is that our time together will inspire you to share what I teach you with even more San Francisco neighbors.  Look forward to a delicious smelling kitchen, and plenty of baked goods to bring home to share.", photo: "photos/turkish_food.png"};
+var eurasian_fusion = {teacher_name:"Margaret", class_name: "Eurasian Fusion", description: "Who says food has borders? Haven't you always wondered the tricks to being a wonderful and skilled chef with any ingredients? I've been cooking and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to move!", photo: "photos/eurasian_food.png"};
+var irish_dance = {teacher_name:"Margaret", class_name:"Irish Dance", description: "Haven't you always wondered the tricks to being a beautiful and skilled irish dancer? I've been dancing and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to move!", photo: "photos/irish_dance.png"};
+var euro_asian_dance =  {teacher_name:"Margaret", class_name:"Euro-Asian Dance", description: "Who says dancing has borders? Haven't you always wondered the tricks to being a beautiful and skilled dancer to any music? I've been dancing and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to move!", photo: "photos/japan.png"};
+var wwII_art = {teacher_name:"Margaret", class_name: "WWII Through Art", description: "Learn history from the perspective of an art historian.  I'll promise you that you will change your perspective about global history after our time together.  I hope you come with an open mind, ready to appreciate the arts", photo: "photos/wwII.png"};
+var turkish_crafts = {teacher_name:"Asli", class_name: "Turkish Crafts", description: "I've been an artist my whole life, and nothing brings me greater joy than teaching about my home culture through art.  Let me know what styles you prefer, and I'll work with you!  Looking forward to meeting you", photo: "photos/turkish_dance.png"};
+var italian_art = {teacher_name:"Margaret", class_name: "Ancient Italian Art", description: "If you want to start learning about art, an afternoon in my studio is the place to start.  My goal is to help you appreceiate the beauty that the great masters left behind for us.  I also think we will have a great time.", photo: "photos/hands.png"};
+var korean_celebrations = {teacher_name:"Asli", class_name: "Korean Celebrations", description: "I've been an artist my whole life, and nothing brings me greater joy than teaching about my home culture through art.  Let me know what styles you prefer, and I'll work with you!  Looking forward to meeting you", photo: "photos/korean_women.png"};
+var south_african_cocktails = {teacher_name:"Nikhita", class_name: "South African Cocktails", description: "Let me share the rich flavors and history of South Africa with you through cocktails.  My hope is that our time together will inspire you to share what I teach you with even more San Francisco neighbors.  Look forward to a fun atmosphere, and plenty of good cheer to bring home to share.", photo: "photos/cocktail.png"};
+var eritrean_cusine = {teacher_name:"Nikhita", class_name: "Eritrean Cuisine", description: "Discover the wonders of greek  cuisine.  My Big Fat Greek Wedding was only the start of all the fun.  Let me know ahead of time if there are any specifics you want to cover, and I'll make sure we're covered. ", photo: "photos/eritrian.png"};
+var injera_etheopia = {teacher_name:"Margaret", class_name: "Etheopian Injera", description: "Come experience the joy of the national food of Etheiopia.  I'll teach you simple recipies, and the art of enjoying the cuisine.  I look forward to our time together, and know we will have a wonderful time.", photo: "photos/etheopian.png"}
+var kpanlogo_ghana ={teacher_name:"Margaret", class_name:"Dance the Kpanlogo", description: "Curious how to dance the beautifil Kpanlogo dance of Ghana? Become a beautiful and skilled dancer in just a few afternoons, and I'll get you started. I've been dancing and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to be inspired!", photo: "photos/kpanlogo.png"};
+var moroccan_dance = {teacher_name:"Nikhita", class_name:"Moroccan Belly Dance", description: "Haven't you always wondered the tricks to being a beautiful and skilled belly dancer? I've been dancing and teaching since my youth, and am excited at the opportunity to share my love of the craft with San Francisco.  All levels welcome, come ready to move!", photo: "photos/turkish_dance.png"};
+var modern_pan_african_dance =  {teacher_name:"Nikhita", class_name:"Pan-African Dance", description: "I've been a dancer my whole life, and nothing brings me greater joy than teaching about my home culture through dance.  Let me know what styles you prefer, and I'll work with you!  Looking forward to meeting you.", photo: "photos/modern.png"};
+var kenyan_jewelry = {teacher_name:"Margaret", class_name: "Kenyan Jewelry", description: "Kenya has a beautiful culture, and to match that beautiful culture, we have beautiful jewels.  In my experence, understanding arts and culture of a country has been the first step in understanding global communities.  I would love to share my love of this particular type of art with you", photo: "photos/kenya.png"};
+var egypt_art = {teacher_name:"Asli", teacher_name:"Margaret", class_name: "Art in Ancient Egypt", description: "Lets take some time to go beyond the pyramids and really uncover the beauty in Egyptian art.  I'll cover the basics and the mysteries left to solve.  Come with questions, and I'll prepare some tea for our afternoon.", photo: "photos/egypt.png"};
+var sushi = {teacher_name:"Asli", class_name: "Sushi Making", description:"I'll teach you how to WOW your friends with make-at-home Sushi.  Here's a secret: it's not that hard. We will talk abou fish selection and tight rolls.  Can't wait!", photo: "photos/sushi.png"}
+
+
+var classes = [];
+classes[4] = p4;
+classes[5] = p5;
+classes[6] = p6;
+classes[7] = p7;
+classes[8] = p8;
+classes[9] = p9;
+
+
+function update(p1, p2, p3){
+	document.getElementById("p1").src = p1.photo;
+	document.getElementById("p2").src = p2.photo;
+	document.getElementById("p3").src = p3.photo
+	document.getElementById("p1").nextSibling.innerHTML = p1.class_name;
+	document.getElementById("p2").nextSibling.innerHTML = p2.class_name;
+	document.getElementById("p3").nextSibling.innerHTML = p3.class_name;
+	classes.push(p1);
+	classes[1]= p1;
+	classes[2]=p2;
+	classes[3]=(p3);
+};
+
+function setup(){
+	var combo = window.location.search.substr(1);
+	var teacher_1 = "Margaret";
+	var teacher_2 = "Asli";
+	var teacher_3 = "Nikhita";
+	if(combo == "EUROPEcooking"){
+			update(greek_food, turkish_food, eurasian_fusion);
+		}else if (combo =="EUROPEdancing"){
+			update(turkish_dance, irish_dance, euro_asian_dance);
+		}else if (combo =="EUROPEart"){
+			update(wwII_art, turkish_crafts, italian_art);
+		}else if (combo =="ASIAcooking"){
+			update(sushi, turkish_food, eurasian_fusion);
+		}else if (combo =="ASIAdancing"){
+			update(turkish_dance, japanese_dance, korean_dance);
+		}else if (combo =="ASIAart"){
+			update(turkish_crafts, wwII_art, korean_celebrations);
+		}else if (combo =="AFRICAcooking"){
+			update(south_african_cocktails, eritrean_cusine, injera_etheopia);
+		}else if (combo =="AFRICAdancing"){
+			update(kpanlogo_ghana, moroccan_dance, modern_pan_african_dance);
+		}else if (combo =="AFRICAart"){
+			update(moroccan_dance, kenyan_jewelry, egypt_art);
+		}
+}
+
+
+function home(){
+	window.location.href = 'main.html';
+}
+
+function home_teach(){
+	window.location.href = 'main_teach.html';
+}
+
+function explore(){
+	var location = document.getElementById("location").value;
+	var subject = document.getElementById("subject").value;
+	var combo = location.concat(subject);
+	window.location.href = 'urbanspire_learn.html?' + combo;
+}
+
+function teach(){
+
+	var location = document.getElementById("location").value;
+	var subject = document.getElementById("subject").value;
+	var combo = location.concat(subject);
+	window.location.href = 'urbanspire_teach.html?' + combo;
+}
+
+function search(){
+	var key = document.getElementById("action").value;
+	if (key =="learn"){
+		var location = document.getElementById("location").value;
+		var subject = document.getElementById("subject").value;
+		var combo = location.concat(subject);
+		window.location.href = 'urbanspire_learn.html?' + combo;
+	}else{
+		window.location.href = 'urbanspire_teach.html';
+	}
+	
+
+}
+
+function subtitle_teach(){
+	var param = window.location.search;
+	var location = "";
+	var skill = "";
+	
+	
+	if(param.charAt(1)=='A'){
+		location = "ASIAN"
+	}else if (param.charAt(1)=='E'){
+		location == "EUROPEAN"
+	}else if (param.charAt(1)=='A'){
+		location == "AFRICAN"
+	}
+
+	
+	if(param.slice(-1) == "t"){
+		skill = "ART";
+	}else if(param.slice(-4) == "king"){
+		skill = "COOKING";
+	}else if(param.slice(-4) == "cing"){
+		skill = "DANCING";
+	}
+
+	var sub = "I WANT TO TEACH " + location + " " + skill + " TO A NEIGHBOR.";
+	document.getElementById("tag").innerHTML = sub;
+
+}
+
+function subtitle_learn(){
+	var param = window.location.search;
+	var location = "";
+	var skill = "";
+	
+	
+
+	if(param.charAt(1)=='A'){
+		location = "ASIAN";
+	}else if (param.charAt(1)=='E'){
+		location = "EUROPEAN";
+	}else if (param.charAt(1)=='A'){
+		location = "AFRICAN";
+	}
+	
+
+	
+	if(param.slice(-1) == "t"){
+		skill = "ART";
+	}else if(param.slice(-4) == "king"){
+		skill = "COOKING";
+	}else if(param.slice(-4) == "cing"){
+		skill = "DANCING";
+	}
+
+	var sub = "I WANT TO LEARN " + location + " " + skill + " FROM A NEIGHBOR.";
+	document.getElementById("tag").innerHTML = sub;
+
+}
+
+
+function subtitle_teach(){
+
+	var param = window.location.search;
+	var location = "";
+	var skill = "";
+
+	if(param.charAt(1)=='A'){
+		location = "ASIAN";
+	}else if (param.charAt(1)=='E'){
+		location = "EUROPEAN";
+	}else if (param.charAt(1)=='A'){
+		location = "AFRICAN";
+	}
+
+
+	
+	if(param.slice(-1) == "t"){
+		skill = "ART";
+	}else if(param.slice(-4) == "king"){
+		skill = "COOKING";
+	}else if(param.slice(-4) == "cing"){
+		skill = "DANCING";
+	}
+
+	var sub = "I WANT TO TEACH " + location + " " + skill + " TO A NEIGHBOR.";
+	document.getElementById("tag").innerHTML = sub;
+
+}
+
+function confirm_new_class(){
+	document.getElementById("confirm_box").style.display = "block";
+}
+
+function close_event(){
+	document.getElementById("event_box").style.display = "none";
+}
+
+function close_confirm(){
+	document.getElementById("confirm_box").style.display = "none";
+}
+
+function confirm(){
+	close_event();
+	document.getElementById("confirm_box").style.display = "block";
+}
+
+document.addEventListener('click', function(event){
+	var target_class = event.target.className;
+	if(target_class == "square"){
+		var index = parseInt(event.target.id.charAt(1));
+		document.getElementById("event_photo").src = classes[index].photo;
+		document.getElementById("teacher_name").innerHTML = "Hello Neighbor! I'm "+ classes[index].teacher_name;
+		document.getElementById("icon").src = "photos/"+classes[index].teacher_name + ".png";
+		document.getElementById("event_name").innerHTML = classes[index].class_name;
+		document.getElementById("event_description").innerHTML = classes[index].description;
+		var box = document.getElementById("event_box")
+		box.style.display = "block";
+	}
+});
